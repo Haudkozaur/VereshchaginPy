7901449fdead951845499d460df7f877176f3ff1
Haudkozaur <julianhaudek@gmail.com>
2025-04-19 19:12:25 +0200

    setup.py update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vereshchaginpy',
-    version='0.1.1',
+    version='0.1.2',
     author='Julian Haudek',
     author_email='julianhaudek@gmail.com',
     description='Graph-based integration for structural mechanics using the Vereshchagin method',
